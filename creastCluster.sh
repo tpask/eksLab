fdcca36a0cde42180bbd5d3d36333ee36f2b808e
Thien <11982736+tpask@users.noreply.github.com>
2020-09-02 13:41:32 -0700

    Update creastCluster.sh

--- creastCluster.sh
+++ creastCluster.sh
@@ -4,8 +4,8 @@ nodes=1
 maxNodes=3
 minNodes=1
 nodeType=t3.micro
-nodeGrpName=standard-workder
-name=dev-cluser
+nodeGrpName=standard-worker
+name=dev-cluster
 
 echo eksctl create cluster --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
 

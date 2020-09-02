04ec39f5f389added01464f001290be774a9b040
Thien <11982736+tpask@users.noreply.github.com>
2020-09-02 15:15:15 -0700

    Update creastCluster.sh

--- creastCluster.sh
+++ creastCluster.sh
@@ -4,9 +4,10 @@ nodes=1
 maxNodes=3
 minNodes=1
 nodeType=t3.micro
+AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 nodeGrpName=standard-worker
 name=dev-cluster
 
-echo eksctl create cluster --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
+echo eksctl create cluster --region ${AWS_REGION} --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
 
-eksctl create cluster --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
+eksctl create cluster --region ${AWS_REGION} --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed

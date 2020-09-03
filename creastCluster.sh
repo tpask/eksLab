41b58efdd894def2483ebab2c73314b05201ec22
Thien <11982736+tpask@users.noreply.github.com>
2020-09-02 21:05:35 -0700

    Update creastCluster.sh

--- creastCluster.sh
+++ creastCluster.sh
@@ -4,10 +4,12 @@ nodes=1
 maxNodes=3
 minNodes=1
 nodeType=t3.small
-AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 nodeGrpName=standard-worker
 name=dev-cluster
+AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 
 echo eksctl create cluster --region ${AWS_REGION} --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
-
 eksctl create cluster --region ${AWS_REGION} --name ${name} --nodegroup-name ${nodeGrpName} --node-type ${nodeType} --nodes ${nodes} --nodes-min ${minNodes} --nodes-max ${maxNodes} --managed
+
+echo updating kube configuration....
+aws eks update-kubeconfig --name ${name} --region ${AWS_REGION}
